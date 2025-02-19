d48aa8875c0fcd7422e897960e221c090f39f65a
mkovacevic10 <max.billroth@gmail.com>
2025-02-19 15:49:28 +0100

    Methoden und Attribute hinzugefügt, umd die Fragen dann hinzufügen

--- Fragen.java
+++ Fragen.java
@@ -1,2 +1,17 @@
 public class Fragen {
+    private String question;
+    private String correctAnswer;
+
+    public Fragen(String question, String correctAnswer) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public boolean isCorrect(String answer) {
+        return answer.equalsIgnoreCase(correctAnswer);
+    }
 }
