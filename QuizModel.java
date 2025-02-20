090aa9cdfc8bd7d0a5aa7bf12211d0c2ea7ff7de
mkovacevic10 <max.billroth@gmail.com>
2025-02-20 11:19:44 +0100

    Mit Wissen von Generics und List: Methoden und Attribute hinzugefügt, um die Fragen zu überprüfen, Punkte auszugeben, Quiz zu reseten, etc.

--- QuizModel.java
+++ QuizModel.java
@@ -1,2 +1,43 @@
+import java.util.List;
+
 public class QuizModel {
+    private List<Fragen> questions;
+    private int currentQuestionIndex;
+    private int score;
+
+    public QuizModel(List<Fragen> questions) {
+        this.questions = questions;
+        this.currentQuestionIndex = 0;
+        this.score = 0;
+    }
+
+    public Fragen getCurrentQuestion() {
+        return questions.get(currentQuestionIndex);
+    }
+
+    public boolean checkAnswer(String answer) {
+        boolean isCorrect = getCurrentQuestion().isCorrect(answer);
+        if (isCorrect) {
+            score++;
+        }
+        currentQuestionIndex++;
+        return isCorrect;
+    }
+
+    public boolean hasMoreQuestions() {
+        return currentQuestionIndex < questions.size();
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getTotalQuestions() {
+        return questions.size();
+    }
+
+    public void resetQuiz() {
+        currentQuestionIndex = 0;
+        score = 0;
+    }
 }
